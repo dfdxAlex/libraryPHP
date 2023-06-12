94eebf8f1b546884791a56287292427e6f578c75
oleksandr-ishchuk <alexmway@mail.ru>
2023-06-12 22:30:12 +0200

    add comment for abstract class APropertyContainer

--- bootstrap/navbar/APropertyContainer.php
+++ bootstrap/navbar/APropertyContainer.php
@@ -1,6 +1,12 @@
 <?php
 namespace bootstrap\navbar;
 
+/**
+ * abstract class to implement the Property Container pattern
+ * 
+ * абстрактный класс для реализации шаблона Property Container
+ */
+
 abstract class APropertyContainer
 {
     protected $property = [];
