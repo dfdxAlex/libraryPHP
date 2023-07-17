6775b2c46f60e3d92b0fd729c27f87f5cfdb0d9d
oleksandr-ishchuk <alexmway@mail.ru>
2023-07-17 20:10:51 +0200

    add new decorator for HTMLHead

---
+++ nonBD/PropertyContainer.php
@@ -0,0 +1,26 @@
+<?php
+namespace class\nonBD;
+
+/**
+ * Класс контейнер свойств, может быть использован любым другим классом.
+ * 
+ * The property container class can be used by any other class.
+ */
+
+class PropertyContainer
+{
+    private $property = [];
+
+    public function getProperty($name)
+    {
+        if (isset($this->property[$name]))
+            return $this->property[$name];
+        else 
+            return false;
+    }
+
+    public function setProperty($name, $value)
+    {
+        $this->property[$name] = $value;
+    }
+}
